e9e6a5fe169ff6a2961033ff670e8cac7432735f
Roger Thomas <rogere84@gmail.com>
2020-12-18 12:19:39 +0000

    Provision changes

--- provision.sh
+++ provision.sh
@@ -1,37 +1,57 @@
 #!/usr/bin/env bash
 
 echo "Beginning provisioning"
-sudo apt-get update
-export LANGUAGE=en_GB.UTF-8
-export LANG=en_GB.UTF-8
-export LC_ALL=en_GB.UTF-8
-locale-gen en_GB.UTF-8
-sudo dpkg-reconfigure locales
-sudo apt-get install -y vim curl python-software-properties
-sudo apt-get install -y nginx php5-fpm
-sudo apt-get install -y php5-memcache memcached php-apc
+if ! grep -q "ubuntu-xenial" /etc/hosts; then
+    echo "127.0.0.1 ubuntu-xenial" >> /etc/hosts
+fi
+
+apt-get update
+
+add-apt-repository ppa:ondrej/php
+add-apt-repository ppa:ondrej/nginx
+
+apt-get update
+apt-get upgrade -y
+apt-get install -y --allow-unauthenticated zip unzip nginx git curl language-pack-en memcached \
+  php7.2-fpm php7.2 php7.2-bcmath php7.2-bz2 php7.2-cli php7.2-curl \
+  php7.2-intl php7.2-fileinfo php7.2-json php7.2-mbstring php7.2-opcache php7.2-soap php7.2-sqlite3 \
+  php7.2-xml php7.2-xsl php7.2-gd php7.2-zip php-memcache \
+  php7.2-memcached
+apt-get update
+apt-get upgrade -y
+
+sed -i -e 's/session.gc_maxlifetime = 1440/session.gc_maxlifetime = 31536000/g' /etc/php/7.2/fpm/php.ini
+sed -i -e 's/session.cookie_lifetime = 0/session.cookie_lifetime = 31536000/g' /etc/php/7.2/fpm/php.ini
+sed -i -e 's/upload_max_filesize = 2M/upload_max_filesize = 30M/g' /etc/php/7.2/fpm/php.ini
+sed -i -e 's/post_max_size = 8M/post_max_size = 30M/g' /etc/php/7.2/fpm/php.ini
+
+service php7.2-fpm stop
+service nginx stop
 
 echo "Configuring nginx"
-sudo rm -rf /var/www
-sudo ln -s /vagrant/web /var/www
-sudo rm /etc/nginx/sites-enabled/default
-cat << EOF | sudo tee -a /etc/nginx/sites-enabled/vagrant-site
-server {
-    listen *:80;
-    root /var/www;
-    index index.php;
-    try_files \$uri \$uri/ /index.php?\$query_string;
-    # see http://stackoverflow.com/questions/12782946/js-and-css-files-in-vagrant-not-properly-encoded-when-saved-outside-of-the-vm
-    sendfile off;
-    location ~ ^/index\.php$ {
-        include fastcgi_params;
-        fastcgi_pass 127.0.0.1:9000;
-        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
-    }
-}
-EOF
+rm -rf /var/www
+ln -s /vagrant/web /var/www
 
-sudo service nginx restart
-sudo service php5-fpm restart
+if [ -f "/etc/nginx/sites-enabled/default" ]; then
+  rm /etc/nginx/sites-enabled/default
+fi
 
+cp /vagrant/nginx.conf /etc/nginx/sites-enabled/vagrant-site
 
+service nginx start
+service php7.2-fpm start
+
+if [[ -e /usr/local/bin/composer ]]; then
+    /usr/local/bin/composer self-update
+else
+    curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+fi
+sleep 2
+if [[ -e /var/www/html ]]; then
+    rm -rf /var/www/html
+fi
+
+usermod -a -G vagrant www-data
+
+service php7.2-fpm restart
+service nginx restart
